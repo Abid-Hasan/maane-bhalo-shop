d303932d1ff7ac4fb33190003c921604a48230c6
Abid-Hasan <ahsnigdho@gmail.com>
2022-01-08 08:36:13 +0600

    Finish Shop UI

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -3,9 +3,20 @@ import "./Product.css";
 import React from "react";
 
 const Product = (props) => {
+  const { name, seller, img, price, star, starCount } = props.product;
   return (
     <div className="product">
-      <h4>{props.product.name}</h4>
+      <div>
+        <img className="product-image" src={img} alt="" />
+      </div>
+      <div className="product-description">
+        <h4 className="product-name">{name}</h4>
+        <p>Seller: {seller}</p>
+        <p>
+          Price: <strong>${price}</strong>
+        </p>
+        <button className="primary-button">Add to cart</button>
+      </div>
     </div>
   );
 };
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -20,6 +20,7 @@ const Shop = () => {
         <div className="cart-container">
           <h3>Order Summary</h3>
           <h5>Items Ordered:</h5>
+          <button className="primary-button">Order Review</button>
         </div>
       </div>
     </div>
